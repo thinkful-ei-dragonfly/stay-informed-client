e97d452ea93fea026380285fef9a9be36e119aff
Lucas Vocos <lucas.vocos@me.com>
2019-08-16 11:54:02 -0400

    create shell representativePage; gather props for child components

--- src/routes/RepresentativeRoute/RepresentativeRoute.js
+++ src/routes/RepresentativeRoute/RepresentativeRoute.js
@@ -8,15 +8,35 @@ export default class RepresentativeRoute extends React.Component {
   static contextType = UserContext;
 
   render() {
-    // let currRepId = this.props.match.params.repId;
-    // TEMP - capture currently rendered rep
+    let currRepId = this.props.match.params.repId;
+    let name = ''
+    let party = ''
+    let currentRole = ''
+    let contribs = ''
+
     if (this.context.representatives) {
-      // const currentRep = this.context.representatives.find( rep => rep.member_id === currRepId)
-      // console.log(`my current rep is ${currentRep.first_name}`)
+      const currentRep = this.context.representatives.find( rep => rep.member_id === currRepId)
+      name = `${currentRep.first_name} ${currentRep.last_name}`
+      currentRole = currentRep.roles[0].title
+      party = currentRep.current_party
+      contribs = currentRep.contributionTotals
+      debugger;
     }
     return (
-      <div className="rep">
-        This is the Representative Route for my currently clicked representative
+      <div className="representativePage">
+        <aside className='reppresentativeInfo'>
+          <h1>{name}</h1>
+          <h2>{currentRole}</h2>
+          <h3>{party}</h3>
+          {/* FinanceComponent goes here*/}
+          {/* FinanceComponent goes here*/}
+          {/* TotalContributions with prop of contribs goes here*/}
+
+        </aside>
+        <div className='representativeImage'>
+          {/* Image would go here */}
+        </div>
+
       </div>
     );
   }
