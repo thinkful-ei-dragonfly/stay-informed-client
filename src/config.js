e3c8ae5883fddbbc1174929c3a87077ef4e66ec5
Dustin Haefele <Dustin.haefele@gmail.com>
2019-08-19 11:48:18 -0400

    fixed double fetch

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-  //API_ENDPOINT: 'http://localhost:8000/api',
-  API_ENDPOINT: 'https://stay-info.herokuapp.com/api',
+  API_ENDPOINT: 'http://localhost:8000/api',
+  //API_ENDPOINT: 'https://stay-info.herokuapp.com/api',
   TOKEN_KEY: 'stay-informed-auth-token',
 }
 
