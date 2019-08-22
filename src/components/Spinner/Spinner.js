53d5df45db5fb7dae386dad0860976c35d17c709
Lucas Vocos <lucas.vocos@me.com>
2019-08-22 12:16:02 -0400

    style the loader

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,30 +1,17 @@
 import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { library } from "@fortawesome/fontawesome-svg-core";
-// import { faUser } from "@fortawesome/free-solid-svg-icons";
-import './Spinner.css';
-// import lottie from 'lottie-web'
+import './Spinner.scss';
 import NewspaperSpinner from './newspaper-spinner.json'
 import Lottie from 'react-lottie';
 
-// library.add(faUser);
-
 export default function Spinner () {
-//   lottie.loadAnimation({
-//   container: ('#animationDiv'), // the dom element that will contain the animation
-//   renderer: 'svg',
-//   loop: true,
-//   autoplay: true,
-//   path: NewspaperSpinner // the path to the animation json
-// });
-// <FontAwesomeIcon icon='user' />
+
 const defaultOptions = {
       loop: true,
       autoplay: true,
       animationData: NewspaperSpinner,
 
     };
-debugger
+
   return (
     <div className='spinner-div' >
       <Lottie className='lottie-player' options={defaultOptions}/>
