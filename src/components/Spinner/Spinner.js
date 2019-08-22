b0b0196d2b1d855a878dac0ac4e0e81c54c6f5c1
Lucas Vocos <lucas.vocos@me.com>
2019-08-22 11:59:51 -0400

    new font, install react-lottie

--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faUser } from "@fortawesome/free-solid-svg-icons";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { library } from "@fortawesome/fontawesome-svg-core";
+// import { faUser } from "@fortawesome/free-solid-svg-icons";
 import './Spinner.css';
+// import lottie from 'lottie-web'
+import NewspaperSpinner from './newspaper-spinner.json'
+import Lottie from 'react-lottie';
 
-library.add(faUser);
+// library.add(faUser);
 
 export default function Spinner () {
+//   lottie.loadAnimation({
+//   container: ('#animationDiv'), // the dom element that will contain the animation
+//   renderer: 'svg',
+//   loop: true,
+//   autoplay: true,
+//   path: NewspaperSpinner // the path to the animation json
+// });
+// <FontAwesomeIcon icon='user' />
+const defaultOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: NewspaperSpinner,
+
+    };
+debugger
   return (
-    <div className='spinner-div'> 
-      <FontAwesomeIcon icon='user' />
+    <div className='spinner-div' >
+      <Lottie className='lottie-player' options={defaultOptions}/>
       <p>Please wait while we find your representatives</p>
     </div>
   )
-} 
+}
