146ed72d8ed2847e697de7a60811477174d99bac
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-08-26 14:00:38 -0400

    checked package dependencies for --production

--- src/index.js
+++ src/index.js
@@ -2,18 +2,11 @@ import 'unfetch/polyfill'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import { UserProvider } from './contexts/UserContext'
 import App from './components/App/App'
 import './index.scss'
 import * as serviceWorker from './serviceWorker'
 
-
-
-library.add (
-  faSpinner,
-)
 
 ReactDOM.render(
   <BrowserRouter>
