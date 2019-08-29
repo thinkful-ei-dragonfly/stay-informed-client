18bc646ca2f3c5ab32a60262af2156ff7056ae1a
Dustin Haefele <Dustin.haefele@gmail.com>
2019-08-29 16:21:45 -0400

    updated number format and added descriptions on mobile

--- src/components/TextContributions/TextContributions.js
+++ src/components/TextContributions/TextContributions.js
@@ -8,10 +8,12 @@ export default class TextContributions extends React.Component {
       if (this.props.contributions[0].org_name) {
         contribHeader = 'Top Contributors'
         contribList = this.props.contributions.map(line => {
+          const total = parseInt(line.total).toLocaleString();
+          console.log(total);
           return (
             <p className='contributionItem' key={line.org_name}>
               <span className='repPage-span'>{line.org_name}</span>
-              <span className='contributionAmount'>${line.total}</span>
+              <span className='contributionAmount'>${total}</span>
             </p>
           )
         })
@@ -19,37 +21,42 @@ export default class TextContributions extends React.Component {
       else {
         contribHeader = 'Top Industries'
         contribList = this.props.contributions.map(line => {
+          const total = parseInt(line.total).toLocaleString();
           if (line.industry_name === 'TV/Movies/Music') {
             return (
               <p className='contributionItem' key={line.industry_name}>
                 <span className='repPage-span'>TV / Movies / Music</span>
-                <span className='contributionAmount'>${line.total}</span>
+                <span className='contributionAmount'>${total}</span>
               </p>
             )
           }
           return (
             <p className='contributionItem' key={line.industry_name}>
               <span className='repPage-span'>{line.industry_name}</span>
-              <span className='contributionAmount'>${line.total}</span>
+              <span className='contributionAmount'>${total}</span>
             </p>
           )
         })
       }
     } else if (this.props.contribs) {
       contribHeader = 'Total Contributions in the last cycle'
+      const total_donations = parseInt(this.props.contribs.total_donations).toLocaleString();
+      const spent = parseInt(this.props.contribs.spent).toLocaleString();
+      const cash_on_hand = parseInt(this.props.contribs.cash_on_hand).toLocaleString();
+
       contribList = (
         <>
         <p className='contributionItem'>
           <span className='repPage-span'>Total Donations</span>
-          <span className='contributionAmount'>${this.props.contribs.total_donations}</span>
+          <span className='contributionAmount'>${total_donations}</span>
         </p>
         <p className='contributionItem'>
           <span className='repPage-span'>Spent {' '} {'  '}</span>
-          <span className='contributionAmount'>${this.props.contribs.spent}</span>
+          <span className='contributionAmount'>${spent}</span>
         </p>
         <p className='contributionItem'>
           <span className='repPage-span'>Cash on Hand</span>
-          <span className='contributionAmount'>${this.props.contribs.cash_on_hand}</span>
+          <span className='contributionAmount'>${cash_on_hand}</span>
         </p>
 
         </>
