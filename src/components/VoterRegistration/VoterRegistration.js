3aba149a4394162f5a436d5815a9c5099443211a
Dustin Haefele <Dustin.haefele@gmail.com>
2019-08-30 15:16:53 -0400

    added fake loading

--- src/components/VoterRegistration/VoterRegistration.js
+++ src/components/VoterRegistration/VoterRegistration.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import './VoterRegistration.css'
+import './VoterRegistration.scss'
 
 export default class VoterRegistration extends Component {
   render() {
     return (
       <div className="reg-vote">
        <Link to="/dashboard" className="dash-link">Navigate back to dashboard</Link>
+       <p class="loading">Loading...</p>
         <iframe
           title="Use this form to register to vote in your local, state, and national elections."
           src="https://register.rockthevote.com/?partner=1&source=ovrpage"
